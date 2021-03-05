dd3a08ee3329c220d2cf31619e42aefd01cde2bb
Muhammad Yasser <mohyas364@gmail.com>
2021-03-05 18:37:29 +0200

    new folder structure, new environment, new techs, V.2

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    prefix = require("gulp-autoprefixer"),
+    sass = require("gulp-sass"),
+    pug = require("gulp-pug"),
+    sourceMaps = require("gulp-sourcemaps"),
+    uglify = require("gulp-uglify"),
+    imagemin = require("gulp-image"),
+    include = require("gulp-include"),
+    livereload = require("gulp-livereload"),
+    zip = require("gulp-zip");
+
+gulp.task("images", () => {
+    return gulp
+        .src("src/imgs/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("build/imgs"))
+        .pipe(livereload());
+});
+
+gulp.task("html", () => {
+    return gulp
+        .src("src/pages/Home/*.pug")
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest("./build"))
+        .pipe(livereload());
+});
+
+gulp.task("styles", () => {
+    return gulp
+        .src("src/styles/*.scss")
+        .pipe(sourceMaps.init())
+        .pipe(sass({ outputStyle: "compressed" }))
+        .pipe(prefix("last 2 versions"))
+        .pipe(concat("style.css"))
+        .pipe(sourceMaps.write("."))
+        .pipe(gulp.dest("./build/css"))
+        .pipe(livereload());
+});
+
+gulp.task("scripts", () => {
+    return gulp
+        .src("src/scripts/*.js")
+        .pipe(include())
+        .pipe(concat("main.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("./build/js"))
+        .pipe(livereload());
+});
+
+gulp.task("compress", () => {
+    return gulp.src("build").pipe(zip("website.zip")).pipe(gulp.dest("."));
+});
+
+gulp.task("watch", () => {
+    livereload.listen();
+    gulp.watch("src/pages/**/*.pug", ["html"]);
+    gulp.watch("src/styles/**/*.scss", ["styles"]);
+    gulp.watch("src/scripts/*.js", ["scripts"]);
+    gulp.watch("build/**/*.*", ["compress"]);
+});
---
+++ src/scripts/main.js
@@ -0,0 +1,58 @@
+/*global $*/
+//=require "./vendors/jquery-3.2.1.min.js"
+//=require "./vendors/fontawesome-all.min.js"
+//=require "./vendors/popper.js"
+//=require "./vendors/wow.min.js"
+//=require "./vendors/bootstrap.min.js"
+//=require "./vendors/jquery.countTo.js"
+//=require "./vendors/jquery.nicescroll.min.js"
+
+$(function () {
+    "use strict";
+
+    // initiate Wow plugin
+    new WOW().init();
+
+    // config and init niceScroll plugin
+    $("html").niceScroll({
+        cursorcolor: "#ee1c25",
+        cursorwidth: "10px",
+        cursorborder: "none",
+        horizrailenabled: false,
+        zindex: 10,
+    });
+
+    // show the return to top button when we scroll down a little
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > $(this).height()) {
+            $(".goup").fadeIn();
+        } else {
+            $(".goup").fadeOut();
+        }
+    });
+
+    // fade out the non-selected work images
+    $(".featured li").click(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        $(".imgs .row div").css("opacity", ".1");
+        $(".imgs .row div" + $(this).data("class")).css("opacity", "1");
+    });
+
+    // toggle active status on links when clicking them
+    $(".navbar li a").click(function () {
+        $(".navbar li a").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    // "return to top" button functionality
+    $(".goup").click(function () {
+        $("html,body").animate({ scrollTop: 0 }, 1500);
+    });
+
+    // make the stats start counting slightly before scroll reaches it
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= $(".stat").offset().top - 500) {
+            $(".stat span:first-of-type").countTo();
+        }
+    });
+});
